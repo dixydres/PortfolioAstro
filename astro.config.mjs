ec0701d3ff681cadd32af35ed23485699d56e21b
dixydres <jeanjose.muller@etu.unilim.fr>
2025-05-20 16:09:58 +0200

    config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://muller-jeanjose.fr',
+  site: 'https://dixydres.github.io',
+  base: '/Portfolio',
   integrations: [],
   markdown: {
     shikiConfig: {
